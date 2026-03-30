de8c74a318caacf571bd84d07fc21f9c86ab6eac
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-30 11:58:57 -0500

    Fix syntax errors in Classroom.cs

--- Classroom.cs
+++ Classroom.cs
@@ -3,7 +3,7 @@
 public class Classroom{
 
     public Dictionary<int, string> CreateTeam(string first, string second, string third){
-        Dictionary<int, string> Team = new Team Dictionary<int, string>
+        Dictionary<int, string> Team = new Team Dictionary<int, string>();
             Team.Add(1, first);
             Team.Add(2, second);
             Team.Add(3, third);
